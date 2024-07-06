7b567dc51174cb477ac627af057fd5b522db90b1
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-07-06 20:10:18 +0300

    besm allah

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -10,6 +10,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     ''' return a tuple '''
-    return(k, v * v)
+    return (k, v * v)
